aee2d07dbf9061c279fb74277064fa9c4f639712
YoungjuneKwon <yjkwon@winm2m.com>
2026-09-22 03:05:20 +0000

    Release as v0.3.0

--- cmd/valetfs/main.go
+++ cmd/valetfs/main.go
@@ -47,7 +47,7 @@ type runtimeState struct {
 
 var cliVerbose bool
 
-const cliVersion = "0.2.2"
+const cliVersion = "0.3.0"
 
 // joinKey is the app-provisioned connection key decoded by `serve --join`.
 type joinKey struct {
